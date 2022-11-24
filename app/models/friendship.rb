05cbef6511d039e028d4363aa5971f29342a31a6
Alessandra Piliero <alessandrapiliero92@gmail.com>
2022-11-24 21:07:57 +0000

    created friendship connection

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,4 +2,6 @@ class Friendship < ApplicationRecord
   validates :status, inclusion: { in: %w(pending accepted declined)}
   belongs_to :asker, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
+
+  
 end
