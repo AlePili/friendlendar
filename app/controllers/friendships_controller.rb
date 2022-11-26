173ff94227b9ff2029872a0e897b26bf03406a6d
Alessandra Piliero <alessandrapiliero92@gmail.com>
2022-11-26 12:51:50 +0000

    created the friendship create action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,4 +2,16 @@ class FriendshipsController < ApplicationController
    
    def add
    end 
+
+   def create
+    @receiver = User.find(params[:receiver_id])
+    @asker = current_user
+    @friendship = Friendship.new
+    @friendship.receiver = @receiver
+    @friendship.asker = @asker
+    @friendship.save!
+  end 
+
+
+
 end
