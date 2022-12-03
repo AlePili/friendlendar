e7a6f1f5abdd8a53bc21eb4bb3f5bf67ab73345f
Adriana Harseva <a.harseva@gmail.com>
2022-12-03 10:46:27 +0000

    added friends events to calendfar

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,10 +2,5 @@ class Friendship < ApplicationRecord
   validates :status, inclusion: { in: %w(pending accepted declined)}
   belongs_to :asker, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
-
-
   validates :asker, uniqueness: { scope: :receiver }
-
-
-
 end
