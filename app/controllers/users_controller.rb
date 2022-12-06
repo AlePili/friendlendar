3db72037ff137e25d1819d0460667f5691d2564d
Alessandra Piliero <alessandrapiliero92@gmail.com>
2022-12-06 21:03:27 +0000

    created show page for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,13 @@ class UsersController < ApplicationController
         @users = User.all
         @friendship = Friendship.new
     end 
+
+    def show
+        @user = User.find(params[:id])
+        @events = Event.where(user_id: @user.id)
+
+        @friends = current_user.askers      
+        @friendship_requests = Friendship.where(asker: current_user, receiver: @user)
+
+    end 
 end
